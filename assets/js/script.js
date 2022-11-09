65770ae078bbd3f9d9067c35f17a943d12dcb38c
Pisen99 <patricia.andersson99@hotmail.com>
2022-11-09 01:30:23 +0000

    feat: added in javascript a popup button with game rules inside of it and styled it in css

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,3 +77,16 @@ function resetGame() {
         clear.remove();
     })
 };
+
+// Rules button
+const rulesPopUp = document.getElementById('rules-container')
+
+function popupContainer() {
+    rulesPopUp.style.visibility = 'visible'
+}
+
+
+
+function popupContainerClose() {
+    rulesPopUp.style.visibility = 'hidden'
+}
